a34d573eaaf8f7cefdd11a944db19c1051dd4609
ribeiro-dev <romilson.2r@gmail.com>
2023-08-01 18:18:35 -0300

    developing: sidebar

--- src/pages/components/Sidebar/index.jsx
+++ src/pages/components/Sidebar/index.jsx
@@ -1,5 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, IconButton, Stack, Typography } from "@mui/material";
+
+import { BiLibrary } from "react-icons/bi";
 import { GoHome, GoSearch } from "react-icons/go";
+import { HiArrowRight, HiPlus } from "react-icons/hi";
 
 import "./style.css";
 
@@ -7,16 +10,16 @@ export default function Sidebar() {
 	const itemBgColor = (theme) => theme.palette.background.default;
 
 	return (
-		<Box gridArea='left-sidebar'>
+		<Box gridArea="left-sidebar" display="flex" flexDirection="column" gap={1}>
 
 			<Box
 				component="nav"
-				className="nav"
+				className="container nav"
 				backgroundColor={itemBgColor}
 				borderRadius={3}
 			>
 				<Box className="nav-item">
-					<GoHome className="icon" fontSize="1.7rem" />
+					<GoHome className="icon" />
 					<Typography
 						variant="body1"
 						className="text"
@@ -34,6 +37,48 @@ export default function Sidebar() {
 						Buscar
 					</Typography>
 				</Box>
+			</Box>
+
+			<Box
+				backgroundColor={itemBgColor}
+				borderRadius={3}
+			>
+				<Box
+					component="header"
+					className="container"
+					display="flex"
+					alignItems="center"
+					justifyContent="space-between"
+				>
+					<Stack direction="row" alignItems="center" spacing={1}>
+						<BiLibrary className="icon" />
+						<Typography className="text">Sua Biblioteca</Typography>
+					</Stack>
+
+					<Stack direction="row" spacing={1}>
+						<IconButton size="small" >
+							<HiPlus />
+						</IconButton>
+						<IconButton size="small" >
+							<HiArrowRight />
+						</IconButton>
+					</Stack>
+				</Box>
+
+				<Box>
+					<Stack direction="row" position="relative">
+						<Stack direction="row" spacing={1} sx={{ overflowX: "hidden" }}>
+							<Chip label="Playlists" />
+							<Chip label="Artistas" />
+							<Chip label="Álbuns" />
+							<Chip label="Álbuns" />
+							<Chip label="Álbuns" />
+						</Stack>
+
+						<Chip label={<HiArrowRight />} sx={{  }} />
+					</Stack>
+				</Box>
+
 			</Box>
 		</Box>
 	);
