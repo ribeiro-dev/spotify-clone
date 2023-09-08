a72ca0a66acffd35033b2d8b9a6a17094e8acf4a
ribeiro-dev <romilson.2r@gmail.com>
2023-09-08 20:58:58 -0300

    add: artist top tracks

--- src/pages/components/Content/index.jsx
+++ src/pages/components/Content/index.jsx
@@ -1,9 +1,54 @@
-import { Box, Stack, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+import { Box, Chip, List, ListItem, ListItemButton, Stack, Typography } from "@mui/material";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import { BiDownArrowCircle } from "react-icons/bi";
+import { BiDotsHorizontalRounded, BiDownArrowCircle } from "react-icons/bi";
+import { BsExplicitFill } from "react-icons/bs";
+import { FaCirclePlay } from "react-icons/fa6";
 import { VscVerifiedFilled } from "react-icons/vsc";
 
 import "./style.css";
+
+function SongItem({ name, duration, explicit, urlImg }) {
+	const durationInMinutes = (duration / 60000).toFixed(2).replace(".", ":");
+
+	return (
+		<ListItem disablePadding>
+			<ListItemButton
+				disableRipple
+				sx={{
+					borderRadius: "4px"
+				}}>
+
+				<Stack
+					direction="row"
+					spacing={2}
+					alignItems="center"
+					width={1}
+				>
+
+					<Stack
+						direction="row"
+						alignItems="center"
+						spacing={2}
+						flexBasis="60%"
+					>
+						<Typography>1</Typography>
+						<Box component="img" src={urlImg} width="40px" height="40px" />
+						<Box>
+							<Typography>{name}</Typography>
+							{ explicit && (
+								<BsExplicitFill color="darkgray" fontSize={13} />
+							)}
+						</Box>
+					</Stack>
+					<Typography className="text-gray" textAlign="right" flexGrow={1}>{durationInMinutes}</Typography>
+				</Stack>
+
+			</ListItemButton>
+		</ListItem>
+	);
+}
 
 export default function Content() {
 
@@ -80,6 +125,89 @@ export default function Content() {
 				</Stack>
 			</Box>
 
+			<Box
+				padding="24px 20px"
+				flex={1}
+				sx={{
+					backgroundColor: "rgb(83, 83, 83)",
+					backgroundImage: "linear-gradient(to bottom, #272626, #222121, #1c1c1c, #171717, #121212)"
+				}}
+			>
+				<Stack direction="row" alignItems="center" spacing={4} mb={2}>
+					<Box
+						sx={{
+							display: "inline-block",
+							cursor: "pointer",
+							"&:hover": {
+								transform: "scale(1.05)"
+							}
+						}}
+					>
+						<FaCirclePlay color="#1db954" fontSize={50} />
+					</Box>
+
+					<Chip
+						label="Seguir"
+						sx={{
+							fontWeight: "bold",
+							border: "1px solid #878787",
+							background: "transparent",
+							"&:hover": {
+								cursor: "pointer",
+								border: "1px solid white",
+							}
+						}}
+					/>
+
+					<Box
+						sx={{
+							color: "#A7A7A7",
+							"& :hover": {
+								color: "white",
+								cursor: "pointer",
+							}
+						}}
+					>
+						<BiDotsHorizontalRounded fontSize={30} />
+					</Box>
+				</Stack>
+
+				<Stack>
+					<Typography fontWeight="bold" variant="h6">Populares</Typography>
+
+					<List mb={2}>
+						<SongItem name="X1" explicit={true} urlImg={"https://placehold.co/40x40"} reproductions={"Reproduções"} duration={188852} />
+						<SongItem name="MINHA CURA" explicit={false} urlImg={"https://placehold.co/40x40"} reproductions={"Reproduções"} duration={150000} />
+						<SongItem name="FOGO E GASOLINA" explicit={true} urlImg={"https://placehold.co/40x40"} reproductions={"Reproduções"} duration={300000} />
+						<SongItem name="BEM MELHOR" explicit={false} urlImg={"https://placehold.co/40x40"} reproductions={"Reproduções"} duration={300000} />
+						<SongItem name="CAOS" explicit={false} urlImg={"https://placehold.co/40x40"} reproductions={"Reproduções"} duration={300000} />
+					</List>
+					<Typography
+						ml={1}
+						component="span"
+						className="text-gray"
+						fontWeight="bold"
+						variant="body2"
+						sx={{
+							display: "contents",
+							"&:hover": {
+								cursor: "default !important",
+								color: "white"
+							}
+						}}
+					>
+						Ver mais
+					</Typography>
+				</Stack>
+			</Box>
 		</Stack>
 	);
 }
+
+
+SongItem.propTypes = {
+	name: PropTypes.string.isRequired,
+	duration: PropTypes.number.isRequired,
+	explicit: PropTypes.bool.isRequired,
+	urlImg: PropTypes.string.isRequired,
+};
