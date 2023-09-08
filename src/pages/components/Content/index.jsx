4ddf54b643cc5bbfec03032ea2c1f95ff1e0b446
ribeiro-dev <romilson.2r@gmail.com>
2023-09-08 11:53:13 -0300

    fix: border radius

--- src/pages/components/Content/index.jsx
+++ src/pages/components/Content/index.jsx
@@ -11,68 +11,75 @@ export default function Content() {
 		<Stack
 			component="main"
 			gridArea="main-view"
-			padding="16px 16px"
-			backgroundColor="gray"
 			borderRadius={1}
 		>
-
-			<Stack
-				direction="row"
-				justifyContent="space-between"
-				alignItems="center"
+			<Box
+				padding="16px"
+				sx={{
+					borderTopLeftRadius: "inherit",
+					borderTopRightRadius: "inherit",
+					backgroundImage: "linear-gradient(to bottom, #4c4c4c, #424242, #383838, #2e2e2e, #252525)"
+				}}
 			>
-				<Stack className="navigation-buttons" direction="row" spacing={1}>
-					<Box className="wrapper">
-						<AiOutlineLeft fontSize="1.3rem" />
-					</Box>
-					<Box className="wrapper">
-						<AiOutlineRight fontSize="1.3rem" />
-					</Box>
-				</Stack>
-
 				<Stack
 					direction="row"
+					justifyContent="space-between"
 					alignItems="center"
-					spacing={1}
-					sx={{
-						backgroundColor: "rgba(0,0,0,0.56)",
-						padding: "6px 10px",
-						borderRadius: "20px",
-						"&:hover": {
-							transform: "scale(1.05)"
-						}
-					}}
 				>
-					<BiDownArrowCircle fontSize={20} />
-					<Typography fontSize={13} fontWeight="bold">Instalar Aplicativo</Typography>
+					<Stack className="navigation-buttons" direction="row" spacing={1}>
+						<Box className="wrapper">
+							<AiOutlineLeft fontSize="1.3rem" />
+						</Box>
+						<Box className="wrapper">
+							<AiOutlineRight fontSize="1.3rem" />
+						</Box>
+					</Stack>
+
+					<Stack
+						direction="row"
+						alignItems="center"
+						spacing={1}
+						sx={{
+							backgroundColor: "rgba(0,0,0,0.56)",
+							padding: "6px 10px",
+							borderRadius: "20px",
+							"&:hover": {
+								transform: "scale(1.05)"
+							}
+						}}
+					>
+						<BiDownArrowCircle fontSize={20} />
+						<Typography fontSize={13} fontWeight="bold">Instalar Aplicativo</Typography>
+					</Stack>
 				</Stack>
-			</Stack>
 
-			<Stack direction="row" alignItems="center" mt={2}>
-				<Box
-					component="img"
-					src="https://i.scdn.co/image/ab67616100005174cb8060f9f0e9249f6df52559"
-					alt="MC Cabelinho"
-					sx={{
-						width: "232px",
-						height: "232px",
-						borderRadius: "50%",
-						boxShadow: "0 4px 60px rgba(0,0,0,.5)",
-						objectFit: "cover"
-					}}
-				/>
+				<Stack direction="row" alignItems="center" mt={2}>
+					<Box
+						component="img"
+						src="https://i.scdn.co/image/ab67616100005174cb8060f9f0e9249f6df52559"
+						alt="MC Cabelinho"
+						sx={{
+							width: "232px",
+							height: "232px",
+							borderRadius: "50%",
+							boxShadow: "0 4px 60px rgba(0,0,0,.5)",
+							objectFit: "cover"
+						}}
+					/>
 
-				<Box ml={3} pt={2}>
-					<Box mb={3}>
-						<Stack direction="row" alignItems="center" spacing={1}>
-							<VscVerifiedFilled className="icon-verified" fontSize={25} />
-							<Typography className="text-gray" variant="body2" fontSize={14}>Artista Verificado</Typography>
-						</Stack>
-						<Typography variant="h1" fontWeight="bold">MC Cabelinho</Typography>
+					<Box ml={3} pt={2}>
+						<Box mb={3}>
+							<Stack direction="row" alignItems="center" spacing={1}>
+								<VscVerifiedFilled className="icon-verified" fontSize={25} />
+								<Typography className="text-gray" variant="body2" fontSize={14}>Artista Verificado</Typography>
+							</Stack>
+							<Typography variant="h1" fontWeight="bold">MC Cabelinho</Typography>
+						</Box>
+						<Typography className="text-gray">9.848.122 ouvintes mensais</Typography>
 					</Box>
-					<Typography className="text-gray">9.848.122 ouvintes mensais</Typography>
-				</Box>
-			</Stack>
+				</Stack>
+			</Box>
+
 		</Stack>
 	);
 }
