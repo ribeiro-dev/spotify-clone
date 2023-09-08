d937cc6fa518260d011b9d34a5e11f23a8816371
ribeiro-dev <romilson.2r@gmail.com>
2023-09-08 02:22:08 -0300

    add: artist data

--- src/pages/components/Content/index.jsx
+++ src/pages/components/Content/index.jsx
@@ -1,6 +1,7 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import { BiDownArrowCircle } from "react-icons/bi";
+import { VscVerifiedFilled } from "react-icons/vsc";
 
 import "./style.css";
 
@@ -45,6 +46,32 @@ export default function Content() {
 					<BiDownArrowCircle fontSize={20} />
 					<Typography fontSize={13} fontWeight="bold">Instalar Aplicativo</Typography>
 				</Stack>
+			</Stack>
+
+			<Stack direction="row" alignItems="center" mt={2}>
+				<Box
+					component="img"
+					src="https://i.scdn.co/image/ab67616100005174cb8060f9f0e9249f6df52559"
+					alt="MC Cabelinho"
+					sx={{
+						width: "232px",
+						height: "232px",
+						borderRadius: "50%",
+						boxShadow: "0 4px 60px rgba(0,0,0,.5)",
+						objectFit: "cover"
+					}}
+				/>
+
+				<Box ml={3} pt={2}>
+					<Box mb={3}>
+						<Stack direction="row" alignItems="center" spacing={1}>
+							<VscVerifiedFilled className="icon-verified" fontSize={25} />
+							<Typography className="text-gray" variant="body2" fontSize={14}>Artista Verificado</Typography>
+						</Stack>
+						<Typography variant="h1" fontWeight="bold">MC Cabelinho</Typography>
+					</Box>
+					<Typography className="text-gray">9.848.122 ouvintes mensais</Typography>
+				</Box>
 			</Stack>
 		</Stack>
 	);
