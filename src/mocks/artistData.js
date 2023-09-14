c1e9ff8fc75a98f2c1b3eaa03af1121e828d3106
ribeiro-dev <romilson.2r@gmail.com>
2023-09-13 21:23:44 -0300

    add: artist mock data

---
+++ src/mocks/artistData.js
@@ -0,0 +1,9 @@
+const artistData = {
+	id: "1WQBwwssN6r8DSjUlkyUGW",
+	name: "MC Cabelinho",
+	verified: true,
+	monthlyListeners: 9848122,
+	imageURL: "https://i.scdn.co/image/ab67616100005174cb8060f9f0e9249f6df52559"
+};
+
+export default artistData;
--- src/pages/components/Content/index.jsx
+++ src/pages/components/Content/index.jsx
@@ -8,6 +8,7 @@ import { FaCirclePlay } from "react-icons/fa6";
 import { VscVerifiedFilled } from "react-icons/vsc";
 
 import "./style.css";
+import artistData from "../../../mocks/artistData";
 import topTracks from "../../../mocks/artistTopTracks";
 
 
@@ -130,8 +131,8 @@ export default function Content() {
 					<Stack direction="row" alignItems="center" mt={2}>
 						<Box
 							component="img"
-							src="https://i.scdn.co/image/ab67616100005174cb8060f9f0e9249f6df52559"
-							alt="MC Cabelinho"
+							src={artistData.imageURL}
+							alt={artistData.name}
 							sx={{
 								width: "232px",
 								height: "232px",
@@ -143,13 +144,15 @@ export default function Content() {
 
 						<Box ml={3} pt={2}>
 							<Box mb={3}>
-								<Stack direction="row" alignItems="center" spacing={1}>
-									<VscVerifiedFilled className="icon-verified" fontSize={25} />
-									<Typography variant="body2" fontSize={14}>Artista Verificado</Typography>
-								</Stack>
-								<Typography variant="h1" fontWeight="bold">MC Cabelinho</Typography>
+								{artistData.verified &&
+									<Stack direction="row" alignItems="center" spacing={1}>
+										<VscVerifiedFilled className="icon-verified" fontSize={25} />
+										<Typography variant="body2" fontSize={14}>Artista Verificado</Typography>
+									</Stack>
+								} 
+								<Typography variant="h1" fontWeight="bold">{artistData.name}</Typography>
 							</Box>
-							<Typography>9.848.122 ouvintes mensais</Typography>
+							<Typography>{artistData.monthlyListeners.toLocaleString()} ouvintes mensais</Typography>
 						</Box>
 					</Stack>
 				</Box>
